afb2020d5a7cfdfba3d58dbd2cca191a93a0a7ef
duchenyu <2033249419@qq.com>
2019-12-18 20:23:45 +0800

    修饰渲染

--- duchenyu-cms/src/main/java/com/duchenyu/cms/controller/admin/AdminController.java
+++ duchenyu-cms/src/main/java/com/duchenyu/cms/controller/admin/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.duchenyu.cms.common.JsonResult;
 import com.duchenyu.cms.pojo.Article;
 import com.duchenyu.cms.pojo.Channel;
 import com.duchenyu.cms.pojo.User;
@@ -79,5 +80,45 @@ public class AdminController {
 		System.out.println(b);
 		return b;
 		
+	}
+	
+	/**
+	 * @Title: addHot  
+	 * @Description: 文章加热
+	 * @param: @param article
+	 * @param: @return      
+	 * @return: boolean      
+	 * @throws
+	 */
+	@RequestMapping("/article/addHot")
+	@ResponseBody
+	public boolean addHot(Article article) {
+		return articleService.addHot(article.getId());
+	}
+	
+	/**
+	 * @Title: delByIds   
+	 * @Description: 批量删除   
+	 * @param: @param ids
+	 * @param: @return      
+	 * @return: JsonResult      
+	 * @throws
+	 */
+	@RequestMapping("delByIds")
+	public @ResponseBody JsonResult delByIds(String ids) {
+		if(ids==null) {
+			return JsonResult.fail(10001, "请选择删除的文章");
+		}
+		//已审核判断
+		boolean isCheck = articleService.isAllCheck(ids);
+		if(!isCheck) {
+			return JsonResult.fail(10001, "请选择未审核的文章删除");
+		}
+		//删除
+		boolean result = articleService.delByIds(ids);
+		if(result) {
+			return JsonResult.sucess();
+		}
+		return JsonResult.fail(500, "未知错误");
 	}
 }
