73af6ae7d54e7ef1e84693a9bd5330192bb75b29
duchenyu <2033249419@qq.com>
2019-12-18 19:35:28 +0800

    文章详情页渲染、首页渲染

--- duchenyu-cms/src/main/java/com/duchenyu/cms/common/AuthUserInterceptor.java
+++ duchenyu-cms/src/main/java/com/duchenyu/cms/common/AuthUserInterceptor.java
@@ -5,16 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class AuthInterceptor implements HandlerInterceptor{
+public class AuthUserInterceptor implements HandlerInterceptor{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		Object userInfo = request.getSession().getAttribute(CmsConstant.UserAdminSessionKey);
+		Object userInfo = request.getSession().getAttribute(CmsConstant.UserSessionKey);
 		if(userInfo!=null) {
 			return true;
 		}
-	    response.sendRedirect("/admin/");
+	    response.sendRedirect("/user/login");
 		return false;
 	}
 
